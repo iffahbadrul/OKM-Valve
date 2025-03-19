0a1e6d0f13fed15e1756164d22a8e7797dc93973
iffahbadrul <iffah@asiaquest.my>
2025-03-19 14:36:42 +0800

    Create attachmentMobile.js

---
+++ attachmentMobile.js
@@ -0,0 +1,28 @@
+(function() {
+    "use strict";
+
+    function validateAttachment(event) {
+        var record = event.record;
+        var leaveType = record.Type_of_Leave?.value;
+        var attachment = record.Attachment?.value || [];
+        var nextStatus = event.nextStatus?.value; 
+
+        console.log("Attachment:", attachment);
+        console.log("Next Status:", nextStatus);
+
+        var requiredLeaveType = ["Medical Leave", "Emergency Leave With Document", "Hospitalization Leave"];
+        var requiredStatus = "Submitted to Approver 1";
+
+        if (requiredLeaveType.includes(leaveType) && nextStatus === requiredStatus && attachment.length === 0) {
+            event.error = "Attachment is required for " + leaveType + " before submitting for approval.";
+        }
+
+        return event;
+    }
+
+    kintone.events.on(['mobile.app.record.edit.submit'], validateAttachment);
+    kintone.events.on(['mobile.app.record.detail.process.proceed'], validateAttachment);
+
+})();
+
+
